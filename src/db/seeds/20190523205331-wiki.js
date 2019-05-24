3ddce6bd857300a78d39662b6ba9252c4a156012
unknown <brianguta2000@yahoo.com>
2019-05-23 20:49:08 -0400

    checkpoint seeding

---
+++ src/db/seeds/20190523205331-wiki.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const faker = require("faker");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    const users = [];
+
+    for(let i = 1 ; i <= 3 ; i++){
+      users.push({
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        role: i-1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });
+    }
+
+    await queryInterface.bulkInsert('Users', users, {});
+
+    const addedUsers = await queryInterface.sequelize.query(
+      `SELECT id from "Users";`
+    );
+
+    const userRows = addedUsers[0];
+
+    let wikis = [];
+
+    for(let i = 1 ; i <= 15 ; i++){
+      wikis.push({
+        title: faker.hacker.phrase(),
+        body: faker.lorem.paragraph(),
+        private: false,
+        userId: userRows[userRows.length-1-i%3].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+
+    return await queryInterface.bulkInsert('Wikis', wikis, {});
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: async (queryInterface) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    await queryInterface.bulkDelete("Wikis", null, {});
+    // queryInterface.bulkDelete("Users", null, {});
+
+  }
+};
