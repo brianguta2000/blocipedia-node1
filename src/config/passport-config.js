934344bcf6f53846ef82ae79ffddb815e61606d3
unknown <brianguta2000@yahoo.com>
2019-05-26 17:54:32 -0400

    CRUD fix

--- src/config/passport-config.js
+++ src/config/passport-config.js
@@ -30,7 +30,7 @@ module.exports = {
       callback(null, user.id);
     });
     passport.deserializeUser((id, callback) => {
-      User.findByPk(id)
+      User.findById(id)
       .then((user) => {
         callback(null, user);
       })
